e190fa8e53aa817f017d326ea22af9d34f9fb5b5
Amir Safari <amir2007.safari@gmail.com>
2025-12-21 20:35:22 +0330

    complete player header file

--- include/player.h
+++ include/player.h
@@ -1,5 +1,26 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <math.h>
+#include "raylib.h"
+#include "map.h"
+
+#define BASE_SPEED 200
+#define BASE_ROT_SPEED 2.5f
+#define PLAYER_RADIUS 10
+
 typedef struct
 {
     Vector2 pos;
     Vector2 dir;
 } Player;
+
+extern Player player;
+
+void DrawPlayer();
+
+void InitMobility(float moveSpeed);
+
+void InitRotation(float rotSpeed);
+
+#endif
