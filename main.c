e261ac20b0267b9118106f02e38a279c876b23bd
Amir Safari <amir2007.safari@gmail.com>
2025-12-21 20:36:48 +0330

    init raylib setup

--- main.c
+++ main.c
@@ -2,3 +2,18 @@
 #include "raylib.h"
 #include "map.h"
 #include "player.h"
+
+int main()
+{
+    InitWindow(COLUMNS * TILE_SIZE, ROWS * TILE_SIZE, "Raycaster");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+    }
+
+    CloseWindow();
+    return 0;
+}
